4cbd0234dec52915d265a161777b6ea5edca8656
Ben Godfrey <ben@insiderx.com>
2021-09-28 15:58:55 -0500

    update with svelte

---
+++ src/components/TestComponent.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let imageUrl;
+    const arr = [];
+    while(arr.length < 100){
+        arr.push({ text: 'This is what a Pokemon looks like', image: imageUrl})
+    }
+</script>
+<div>
+    <ul>
+        {#each arr as poke}
+            <li>
+                <img src={poke.image} height=150 width=150 />
+                {poke.text}
+            </li>
+        {/each}
+    </ul>
+</div>
